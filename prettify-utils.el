2b95dd947cb229e7400a7e10c2ef9f4ebf130c09
Ilazki <ws.ilazki@gmail.com>
2019-05-24 23:38:29 -0400

    Added convenience macro prettify-utils-add-hook and updated docstrings.

--- prettify-utils.el
+++ prettify-utils.el
@@ -6,7 +6,7 @@
 ;; Created:             31 Oct 2016
 ;; URL:                 https://github.com/Ilazki/prettify-utils.el
 ;; Keywords:            lisp tools prettify utils prettify-symbols
-;; Version:             1.0.1
+;; Version:             1.0.2
 ;; Package-Version:     20161110.0430
 ;; Package-Requires:    ((emacs "24.4"))
 
@@ -44,8 +44,16 @@
 ;;
 ;; 	(add-hook 'prog-mode-hook (lambda ()
 ;; 								(setq prettify-symbols-alist
-;; 									  (prettify-utils-generate
-;; 									   ("lambda" "λ")))))
+;; 								  (prettify-utils-generate
+;; 								   ("lambda" "λ")))
+;;                              (prettify-symbols-mode 1)))
+;;
+;; For convenience, a macro named prettify-utils-add-hook is also available
+;; that generates the above automatically.
+;; Example:
+;;
+;; (prettify-utils-add-hook prog-mode-hook
+;;                          ("lambda" "λ")))
 ;;
 ;; Or, if you're using the same alist for multiple modes, you can create a named
 ;; function and pass that to the mode hooks:
@@ -58,8 +66,9 @@
 ;;			 ("=>" "⇒"))))
 ;;	(add-hook 'prog-mode-hook 'my-symbols-alist)
 ;;
-;; Generally, prettify-utils-generate should be the only thing needed, but some
-;; additional helpers are provided in case a need arises:
+;; Generally, prettify-utils-generate and prettify-utils-add-hook should be the
+;; only things needed, but some additional helpers are provided in case a need
+;; arises:
 ;;
 ;; * prettify-utils-generate-f is the function equivalent of using
 ;;   prettify-utils-generate.  Should only be needed for creating higher-order
@@ -81,14 +90,14 @@
 (defun prettify-utils--list (l &optional glue)
   "Takes two lists and interleaves the (optional) second between each element of 
 the first.  Used to create multi-character sequences for use with the minor mode
-'prettify-symbols'.  If not supplied, GLUE defaults to '(Br . Bl).  For more 
-information about GLUE, refer to the documentation for the 'compose-region
-function and the 'reference-point-alist variable.
+`prettify-symbols-mode'.  If not supplied, GLUE defaults to '(Br . Bl).  For more 
+information about GLUE, refer to the documentation for the `compose-region'
+function and the `reference-point-alist' variable.
 
-This function is used by prettify-utils-string to create the lists given to 
-prettify-symbols-alist.  Calling prettify-utils--list directly is probably not 
-what you want, check the documentation for prettify-utils-string and 
-prettify-utils-generate instead.
+This function is used by `prettify-utils-string' to create the lists given to 
+`prettify-symbols-alist'.  Calling prettify-utils--list directly is probably not 
+what you want, check the documentation for `prettify-utils-string' and 
+`prettify-utils-generate' instead.
 
 Example use:
 (prettify-utils--list (string-to-list \"hello\") '(Br . Bl))
@@ -108,14 +117,14 @@ Example use:
 (defun prettify-utils-string (s &optional glue)
   "Takes a string and an optional list, and returns a list of the string's 
 characters with GLUE interleaved between each character, for use with 
-prettify-symbols mode.  If no GLUE is supplied, uses the 
-prettify-utils--list default.  For more information about GLUE, refer to the 
-documentation for the 'compose-region function and the 'reference-point-alist 
+`prettify-symbols-mode'.  If no GLUE is supplied, uses the 
+`prettify-utils--list' default.  For more information about GLUE, refer to the 
+documentation for the `compose-region' function and the `reference-point-alist' 
 variable.
 
 This function can be used to simplify multiple-character replacements when 
-manually constructing a prettify-symbols-alist.  For something more high-level, 
-consider using prettify-utils-generate to create the entire alist instead.
+manually constructing a `prettify-symbols-alist'.  For something more high-level, 
+consider using `prettify-utils-generate' to create the entire alist instead.
 
 Example:
 
@@ -126,11 +135,11 @@ Example:
 ;; Was used during macro creation then removed
 (defun prettify-utils-create-pair (old new &optional glue)
   "Takes two strings, OLD and NEW, and an optional GLUE list, and creates an 
-alist pair for use when creating a prettify-symbols-alist.  For more information 
-about GLUE, refer to the documentation for the 'compose-region function and the 
-'reference-point-alist variable.
+alist pair for use when creating a `prettify-symbols-alist'.  For more information 
+about GLUE, refer to the documentation for the `compose-region' function and the 
+`reference-point-alist' variable.
 
-This function is primarily for use by the user-friendly 'prettify-utils-generate
+This function is primarily for use by the user-friendly `prettify-utils-generate'
 macro, but may be useful if manual alist creation is desired for some reason.
 
 Example:
@@ -141,13 +150,13 @@ Example:
   (cons old (prettify-utils-string new glue)))
 
 ;;;###autoload
-(defmacro prettify-utils-generate (&rest pairs)
-  "Generates an alist for use when setting prettify-symbols-alist.  Takes one or 
-more lists, each consisting of two strings and an optional GLUE list to be 
+(defmacro prettify-utils-generate (&rest lists)
+  "Generates an alist for use when setting `prettify-symbols-alist'.  Takes one or 
+more LISTS, each consisting of two strings and an optional GLUE list to be 
 interleaved between characters in the replacement list.  If the optional GLUE
-list is not supplied, uses the prettify-list default of '(Br . Bl).  For more 
-information about GLUE, refer to the documentation for the 'compose-region
-function and the 'reference-point-alist variable.
+list is not supplied, uses the `prettify-utils--list' default of '(Br . Bl).  For more 
+information about GLUE, refer to the documentation for the `compose-region'
+function and the `reference-point-alist' variable.
 
 Example #1:
 
@@ -159,7 +168,7 @@ Example #1:
 Example #2:
 
 (setq prettify-symbols-alist
-	  (prettify-generate 
+	  (prettify-utils-generate 
 	   (\"lambda\"  \"λ\")
 	   (\"|>\"      \"▷\")
 	   (\"<|\"      \"◁\")
@@ -170,8 +179,8 @@ Example #2:
 	   (\"<=\"      \"≤\")
 	   (\">=\"      \"≥\")))
 "
-  (let* ((head       (car   pairs))
-         (tail       (cdr   pairs))
+  (let* ((head       (car   lists))
+         (tail       (cdr   lists))
          (old-string (car   head))
 		 (new-string (cadr  head))
 		 (glue-list  (caddr head)))
@@ -182,17 +191,17 @@ Example #2:
 
 
 ;;;###autoload
-(defun prettify-utils-generate-f (&rest pairs)
-  "Generates an alist for use when setting prettify-symbols-alist.  Takes one or 
-more lists, each consisting of two strings and an optional GLUE list to be 
+(defun prettify-utils-generate-f (&rest lists)
+  "Generates an alist for use when setting `prettify-symbols-alist'.  Takes one or 
+more LISTS, each consisting of two strings and an optional GLUE list to be 
 interleaved between characters in the replacement list.  If the optional GLUE
-list is not supplied, uses the prettify-list default of '(Br . Bl).  For more 
-information about GLUE, refer to the documentation for the 'compose-region
-function and the 'reference-point-alist variable.
+list is not supplied, uses the `prettify-utils--list' default of '(Br . Bl).  For more 
+information about GLUE, refer to the documentation for the `compose-region'
+function and the `reference-point-alist' variable.
 
-This is a function equivalent of the prettify-utils-generate macro.  Unless
+This is a function equivalent of the `prettify-utils-generate' macro.  Unless
 you specifically need a function, such as for use with a higher-order function,
-you should use the 'prettify-utils-generate macro instead.
+you should use the `prettify-utils-generate' macro instead.
 
 Example:
 
@@ -200,8 +209,8 @@ Example:
 				           '(\">=\" \"≥\" (Br . Bl))
 						   '(\"->\"     \"→ \"))
 "
-  (let* ((head       (car   pairs))
-         (tail       (cdr   pairs))
+  (let* ((head       (car   lists))
+         (tail       (cdr   lists))
          (old-string (car   head))
 		 (new-string (cadr  head))
 		 (glue-list  (caddr head)))
@@ -209,5 +218,26 @@ Example:
 	  '()
       (cons (prettify-utils-create-pair old-string new-string glue-list)
                    (apply 'prettify-utils-generate-f tail)))))
+
+;;;###autoload
+(defmacro prettify-utils-add-hook (mode &rest lists)
+  "Convenience macro for the most likely use case of prettify-utils: using
+`add-hook' to add LISTS to MODE. LISTS consists of one or more lists of
+replacements, defined as expected by `prettify-utils-generate'.
+
+Example:
+
+;; Replace org-mode checkboxes with appropriate unicode boxes
+(prettify-utils-add-hook org-mode
+                         (\"[ ]\" \"☐\")
+                         (\"[X]\" \"☑\")
+                         (\"[-]\" \"❍\"))
+
+"
+  `(add-hook ',(intern (concat (symbol-name mode) "-hook"))
+             (lambda ()
+               (setq prettify-symbols-alist
+                     (prettify-utils-generate ,@lists))
+               (prettify-symbols-mode 1))))
 
 ;;; prettify-utils.el ends here
